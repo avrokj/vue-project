1a7a738e16aabc2ab81a78fdb502f7fccb262ff2
avrokj <jan.korva@gmail.com>
2023-09-05 13:49:35 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { fakeStoreAPI } from '../api/fakeStore';
+import { onMounted, ref } from 'vue';
+
+const products = ref(null);
+
+onMounted(async () => {
+  products.value = await fakeStoreAPI.get('/products');
+});
+
+</script>
+
+<template>
+
+</template>
