9d51ad1d785161dbe16e04e5c8e02b36bcaa9fc9
avrokj <jan.korva@gmail.com>
2023-09-05 14:40:41 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,16 @@ import { onMounted, ref } from 'vue';
 const products = ref(null);
 
 onMounted(async () => {
-  products.value = await fakeStoreAPI.get('/products');
+  products.value = await fakeStoreAPI.get("/products");
 });
 
 </script>
 
 <template>
-
+  <article v-for="product in products?.data" :key="product.id"> <!-- toode toodete seas -->
+    <img :src="product.image" alt="" >
+    <h4>{{ product.title }}</h4>
+    <p>{{ product.description }}</p>
+    <p>Hind: {{ product.price }}</p>
+  </article>
 </template>
